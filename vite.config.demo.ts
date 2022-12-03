37b679db4a97fd7534ab93a3ff2f9b17f65f495f
Mims H. Wright <mims@mimswright.com>
2022-12-04 00:14:31 +0100

    fix(build): fixed issues with gh-pages (for real this time)

--- vite.config.demo.ts
+++ vite.config.demo.ts
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/pixi-easing-graph/",
   build: {
     rollupOptions: {
-      output: { dir: "docs" },
+      output: { dir: "demo" },
     },
   },
   plugins: [react()],
